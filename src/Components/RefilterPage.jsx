9a1b6aceda5e7554dc4ab64f5d951f61776ceb2e
MsloFmiTeam <96289155+MsloFmiTeam@users.noreply.github.com>
2021-12-20 01:03:59 +0200

    Update RefilterPage.jsx

--- src/Components/RefilterPage.jsx
+++ src/Components/RefilterPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const RefilterPage = () => {
-    let navigate= useNavigate();
+    let navigate = useNavigate();
     return (
         <div>
             Rezultate Refiltrate dupa orase
